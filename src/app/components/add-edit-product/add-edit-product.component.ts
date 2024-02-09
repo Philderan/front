da75a5290a90c24fe242a2ecbefed2ebde87760b
Federico Ramos <cruci.analistafuncional@gmail.com>
2024-02-09 15:55:18 -0300

    Formulario de Carga de Ingredientes

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-edit-product',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.css'
 })
